207d2bbae88b9b4165e561a1ae6ac7f8404eeacc
docvikts-code <docvikts@gmail.com>
2026-01-29 20:36:08 +0200

    sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,15 @@
-const CACHE_NAME = 'voice-remind-v1';
-const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json'
-];
-
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open('assistant-v1').then((cache) => {
+      return cache.addAll(['index.html', 'manifest.json']);
+    })
+  );
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
 });
